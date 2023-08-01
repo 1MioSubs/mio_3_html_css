c4f03bae695254c60ac000ce66da83f15ed6a54b
1MioSubs <it09022022@gmail.com>
2023-08-01 15:10:29 +0300

    adaptiv header fin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function () {
-  var mixer = mixitup(".portfolio__galeri-all");
+  
 
   $(".slider__inner").slick({
     dots: true,
     arrows: false,
   });
+
+  $(".mune__btn, .menu a").on("click", function () {
+    $(".header__top-inner").toggleClass("header__top-inner--active");
+  });
+
+  var mixer = mixitup(".portfolio__galeri-all");
 });
